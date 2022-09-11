bb45278a6958818c9a90150950da6367c4aeefdf
Henrique Galdino <102200241+Henrique031@users.noreply.github.com>
2022-09-11 12:42:14 -0300

    atualização

--- java/aula07Ex2/Exercicio2/src/Exercicio02.java
+++ java/aula07Ex2/Exercicio2/src/Exercicio02.java
@@ -1,8 +1,11 @@
 // import java.util.Scanner;
 
 public class Exercicio02 {
-    public static void main(String[] args) throws Exception {
-        int numIni = 2, numFim = 10;
+    public static void main(String[] args) {
+        int numIni = 2;
+        int numFim = 10;
+
+        // int numFim = 10;
         /*
          * try (Scanner entradaDados = new Scanner(System.in)) {
          * 
@@ -15,21 +18,26 @@ public class Exercicio02 {
          * }
          */
 
-        int incPar = numIni;
+        // 2
+        // [0]
+        int[] lista = new int[numFim];
+        numFim -= 1;
+        for (int i = 0; i < numFim; i++, numIni++) {
+            lista[i] = numIni;
+        }
 
         System.out.println("Números Pares\n");
-        if (numIni % 2 == 0) {
-            while (incPar <= numFim) {
-                System.out.println(incPar);
-                incPar += 2;
+        for (int i = 1; i < lista.length; i++) {
+            if (lista[i - 1] % 2 == 0) {
+                System.out.println(lista[i - 1]);
             }
         }
-        numIni += 1;
         System.out.println("");
         System.out.println("Números Ímpares\n");
-        while (numIni <= numFim) {
-            System.out.println(numIni);
-            numIni += 2;
+        for (int i = 0; i < lista.length; i++) {
+            if (lista[i] % 2 != 0) {
+                System.out.println(lista[i]);
+            }
         }
     }
 }
