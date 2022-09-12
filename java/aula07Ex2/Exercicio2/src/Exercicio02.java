a089d1c385771741e750e8d73c9ac8f4190cf76a
Henrique Galdino <102200241+Henrique031@users.noreply.github.com>
2022-09-12 09:30:26 -0300

    .

--- java/aula07Ex2/Exercicio2/src/Exercicio02.java
+++ java/aula07Ex2/Exercicio2/src/Exercicio02.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Exercicio02 {
     public static void main(String[] args) {
-        int numIni = 10;
-        int numFim = 1000;
+        int numIni = 0;
+        int numFim = 0;
         int qtdPar = 0;
         int qtdImpar = 0;
 
@@ -12,66 +12,67 @@ public class Exercicio02 {
             System.out.println("Programa do Par e do Ímpar");
 
             // Entrar Inicíal
-            // System.out.print("Digite o número inicíal entre [0 e 500]: ");
-            // numIni = entradaDados.nextInt();
+            System.out.print("Digite o número inicíal entre [0 e 500]: ");
+            numIni = entradaDados.nextInt();
             // Validação Inicíal
-            /*
-             * while (numIni < 0 || numIni > 500) {
-             * System.out.print("[ERRO], Digite um número entre 0 e 500: ");
-             * numIni = entradaDados.nextInt();
-             * }
-             * while (numIni == 0) {
-             * System.out.print("[ERRO], Nenhum dado encontrado por favor, preencha: ");
-             * numIni = entradaDados.nextInt();
-             * }
-             */
+        
+            while (numIni < 0 || numIni > 500) {
+            System.out.print("[ERRO], Digite um número entre 0 e 500: ");
+            numIni = entradaDados.nextInt();
+            }
+            while (numIni == 0) {
+            System.out.print("[ERRO], Nenhum dado encontrado por favor, preencha: ");
+            numIni = entradaDados.nextInt();
+            }
+            
             // Entrada Final
-            // System.out.print("Digite o número final entre [100 e 1000]: ");
-            // numFim = entradaDados.nextInt();
+            System.out.print("Digite o número final entre [100 e 1000]: ");
+            numFim = entradaDados.nextInt();
             // Validação Final
-            /*
-             * while (numFim < numIni) {
-             * System.out.println("[ERRO], Por favor, digite um núemro maior que " + numIni
-             * + " e maior e entre 100 e 1000" + ": ");
-             * numFim = entradaDados.nextInt();
-             * }
-             * while (numIni == numFim) {
-             * System.out.println("[ERRO], Números iguais, por favor digite novamente: ");
-             * numFim = entradaDados.nextInt();
-             * }
-             * while (numFim < 100 || numFim > 1000) {
-             * System.out.println("[ERRO], Digite um valor entre 100 e 1.000: ");
-             * numFim = entradaDados.nextInt();
-             * }
-             * while (numFim == 0) {
-             * System.out.print("[ERRO], Nenhum dado encontrado por favor, preencha: ");
-             * numFim = entradaDados.nextInt();
-             * }
-             */
+        
+            while (numFim < numIni) {
+            System.out.println("[ERRO], Por favor, digite um núemro maior que " + numIni
+            + " e maior e entre 100 e 1000" + ": ");
+            numFim = entradaDados.nextInt();
+            }
+            while (numIni == numFim) {
+            System.out.println("[ERRO], Números iguais, por favor digite novamente: ");
+            numFim = entradaDados.nextInt();
+            }
+            while (numFim < 100 || numFim > 1000) {
+            System.out.println("[ERRO], Digite um valor entre 100 e 1.000: ");
+            numFim = entradaDados.nextInt();
+            }
+            while (numFim == 0) {
+            System.out.print("[ERRO], Nenhum dado encontrado por favor, preencha: ");
+            numFim = entradaDados.nextInt();
+            }
+            
 
         }
 
         // Laços de repetição
-        int[] lista = new int[numFim -= 1];
-        for (int i = 0; i < numFim; i++, numIni++)
-            lista[i] = numIni;
+        int[] lista = new int[numFim];
+        for (int i = 1; i < numFim; i++, numIni++)
+            lista[i-1] = numIni;
 
+        
         System.out.println("");
         System.out.println("Lista de Números Pares\n");
-        for (int i = 1; i <= lista.length; i++) {
-            if (lista[i - 1] % 2 == 0) {
+        for (int i = lista[0]; i <= lista.length; i++) {
+            if (lista[i-lista[0]] % 2 == 0) {
                 qtdPar += 1;
-                System.out.println(lista[i - 1]);
+                System.out.println(lista[i - lista[0]]);
             }
         }
         System.out.println("");
         System.out.println("Qtd de números Pares encontrados: " + qtdPar);
         System.out.println("");
         System.out.println("Lista de Números Ímpares\n");
-        for (int i = 0; i <= lista.length; i++) {
-            if (lista[i] % 2 != 0) {
+        for (int i = lista[0]; i <= lista.length; i++) {
+            if (lista[i-lista[0]] % 2 != 0) {
                 qtdImpar += 1;
-                System.out.println(lista[i]);
+                System.out.println(lista[i-lista[0]]);
             }
         }
         System.out.println("");
