4aa35c1d6b62ad8db4c05b47e651e39a7cb32d59
Henrique Galdino <102200241+Henrique031@users.noreply.github.com>
2022-09-13 01:54:43 -0300

    .

--- java/aula07Ex01/src/br/com/fecaf/parImparEx02/ParImpar.java
+++ java/aula07Ex01/src/br/com/fecaf/parImparEx02/ParImpar.java
@@ -8,61 +8,65 @@ public class ParImpar {
         int numFim = 0;
         int qtdPar = 0;
         int qtdImpar = 0;
+        
+        System.out.println("Programa do Par e do Ímpar");
 
         try (Scanner entradaDados = new Scanner(System.in)) {
-
-            System.out.println("Programa do Par e do Ímpar");
-
             // Entrar Inicíal
             System.out.print("Digite o número inicíal entre [1 e 500]: ");
             numIni = entradaDados.nextInt();
-            // Validação Inicíal
-        
-            while (numIni < 0 || numIni > 500) {
-            System.out.print("[ERRO], Digite um número entre 1 e 500: ");
-            numIni = entradaDados.nextInt();
+            // Validação
+            while (numIni < 0 || numIni > 500 || numIni == 0) {
+
+                while (numIni == 0) {
+                    System.out.print("[ERRO], Nenhum dado encontrado por favor, preencha: ");
+                    numIni = entradaDados.nextInt();
+                }
+
+                while (numIni < 0 || numIni > 500) {
+                    System.out.print("[ERRO], Digite um número entre 1 e 500: ");
+                    numIni = entradaDados.nextInt();
+                }
+
             }
-            while (numIni == 0) {
-            System.out.println("[ERRO], Nenhum dado encontrado por favor, preencha: ");
-            numIni = entradaDados.nextInt();
-            }
-            
+
             // Entrada Final
             System.out.print("Digite o número final entre [100 e 1000]: ");
             numFim = entradaDados.nextInt();
             // Validação Final
-        
-            while (numFim < numIni) {
-            System.out.println("[ERRO], Por favor, digite um núemro maior que " + numIni
-            + " e maior e entre 100 e 1000" + ": ");
-            numFim = entradaDados.nextInt();
+
+            while (numFim < numIni || numIni == numFim || numFim < 100 || numFim > 1000 || numFim == 0) {
+
+                while (numFim < numIni) {
+                    System.out.println("[ERRO], Por favor, digite um núemro maior que " + numIni
+                    + " e maior e entre 100 e 1000" + ": ");
+                    numFim = entradaDados.nextInt();
+                }
+                while (numIni == numFim) {
+                    System.out.println("[ERRO], Números iguais, por favor digite novamente: ");
+                    numFim = entradaDados.nextInt();
+                }
+                while (numFim < 100 || numFim > 1000) {
+                    System.out.println("[ERRO], Digite um valor entre 100 e 1.000: ");
+                    numFim = entradaDados.nextInt();
+                }
+                while (numFim == 0) {
+                    System.out.print("[ERRO], Nenhum dado encontrado por favor, preencha: ");
+                    numFim = entradaDados.nextInt();
+                }
             }
-            while (numIni == numFim) {
-            System.out.println("[ERRO], Números iguais, por favor digite novamente: ");
-            numFim = entradaDados.nextInt();
-            }
-            while (numFim < 100 || numFim > 1000) {
-            System.out.println("[ERRO], Digite um valor entre 100 e 1.000: ");
-            numFim = entradaDados.nextInt();
-            }
-            while (numFim == 0) {
-            System.out.print("[ERRO], Nenhum dado encontrado por favor, preencha: ");
-            numFim = entradaDados.nextInt();
-            }
-            
 
         }
 
         // Laços de repetição
         int[] lista = new int[numFim];
         for (int i = 1; i < numFim; i++, numIni++)
-            lista[i-1] = numIni;
-
+        lista[i - 1] = numIni;
         
         System.out.println("");
         System.out.println("Lista de Números Pares\n");
         for (int i = lista[0]; i <= lista.length; i++) {
-            if (lista[i-lista[0]] % 2 == 0) {
+            if (lista[i - lista[0]] % 2 == 0) {
                 qtdPar += 1;
                 System.out.println(lista[i - lista[0]]);
             }
@@ -72,9 +76,9 @@ public class ParImpar {
         System.out.println("");
         System.out.println("Lista de Números Ímpares\n");
         for (int i = lista[0]; i <= lista.length; i++) {
-            if (lista[i-lista[0]] % 2 != 0) {
+            if (lista[i - lista[0]] % 2 != 0) {
                 qtdImpar += 1;
-                System.out.println(lista[i-lista[0]]);
+                System.out.println(lista[i - lista[0]]);
             }
         }
         System.out.println("");
--- java/aula07Ex01/src/br/com/fecaf/tabuadaEx01/Tabuada.java
+++ java/aula07Ex01/src/br/com/fecaf/tabuadaEx01/Tabuada.java
@@ -3,64 +3,80 @@ package aula07Ex01.src.br.com.fecaf.tabuadaEx01;
 import java.util.Scanner;
 
 public class Tabuada {
-    public static void main(String [] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+
         int tabIni = 0, tabFim = 0, cIni = 0, cFim = 0;
+
         try (Scanner entradaDados = new Scanner(System.in)) {
+
             System.out.print("Insira a tabuada de inicío: ");
             tabIni = entradaDados.nextInt();
-            while (tabIni == 0) {
-                System.out.println("Nenhum dado indentificado, digite pelo menos um valor: .");
-                tabIni = entradaDados.nextInt();
-            }
-            while (tabIni <= 1 || tabFim >= 101) {
-                System.out.println("Digite um valor entre 2 e 100: ");
-                tabIni = entradaDados.nextInt();
+
+            while (tabIni == 0 || (tabIni <= 1 || tabIni >= 101)) {
+
+                while (tabIni == 0) {
+                    System.out.println("Nenhum dado indentificado, digite pelo menos um valor: .");
+                    tabIni = entradaDados.nextInt();
+                }
+                while (tabIni <= 1 || tabIni >= 101) {
+                    System.out.println("Digite um valor entre 2 e 100: ");
+                    tabIni = entradaDados.nextInt();
+                }
             }
 
             System.out.print("Insira a tabuada de fim: ");
             tabFim = entradaDados.nextInt();
-            while (tabIni == tabFim) {
-                System.out.println("Dados repetidos, digite novamente: ");
-                tabFim = entradaDados.nextInt();
-            }
-            while (tabFim == 0) {
-                System.out.println("Nenhum dado encontrado, digite novamente: ");
-                tabFim = entradaDados.nextInt();
-            }
-            while (tabFim <= tabIni || tabFim >= 100) {
-                System.out.println("Digite um valor a cima de " + tabIni + " e até 100: ");
-                tabFim = entradaDados.nextInt();
+            while (tabIni == tabFim || tabFim == 0 || (tabFim <= tabIni || tabFim >= 100)) {
+
+                while (tabIni == tabFim) {
+                    System.out.println("Dados repetidos, digite novamente: ");
+                    tabFim = entradaDados.nextInt();
+                }
+                while (tabFim == 0) {
+                    System.out.println("Nenhum dado encontrado, digite novamente: ");
+                    tabFim = entradaDados.nextInt();
+                }
+                while (tabFim <= tabIni || tabFim >= 100) {
+                    System.out.println("Digite um valor a cima de " + tabIni + " e até 100: ");
+                    tabFim = entradaDados.nextInt();
+                }
             }
 
             System.out.print("Insira o inicío do contador: ");
             cIni = entradaDados.nextInt();
-            while (cIni == cFim) {
-                System.out.println("Valor repetido, digite novamente: ");
-                cIni = entradaDados.nextInt();
-            }
-            while (cIni == 0) {
-                System.out.println("Nenhum dado encontrado, digite novamente: ");
-                cIni = entradaDados.nextInt();
-            }
-            while (cIni < 0 || cIni >= 51) {
-                System.out.println("Insira um valor entre 0 e 50: ");
-                cIni = entradaDados.nextInt();
+            while (cIni == cFim || cIni == 0 || (cIni < 0 || cIni >= 51)) {
+
+                while (cIni == cFim) {
+                    System.out.println("Valor repetido, digite novamente: ");
+                    cIni = entradaDados.nextInt();
+                }
+                while (cIni == 0) {
+                    System.out.println("Nenhum dado encontrado, digite novamente: ");
+                    cIni = entradaDados.nextInt();
+                }
+                while (cIni < 0 || cIni >= 51) {
+                    System.out.println("Insira um valor entre 0 e 50: ");
+                    cIni = entradaDados.nextInt();
+                }
             }
 
             System.out.print("Insira o fim do contador: ");
             cFim = entradaDados.nextInt();
-            while (cIni == cFim) {
-                System.out.println("Valor repetido! \nDigite novamente: ");
-            }
-            while (cFim == 0) {
-                System.out.println("Nenhum dado encontrado! Digite novamente: ");
-                cFim = entradaDados.nextInt();
-            }
-            while (cFim < cIni || cFim >= 50) {
-                System.out.println("Digite um valor a cima de " + cIni + " e até 50: ");
-                cFim = entradaDados.nextInt();
-            }
+            while (cIni == cFim || cFim == 0 || (cFim < cIni || cFim >= 50)) {
 
+                while (cIni == cFim) {
+                    System.out.println("Valor repetido! \nDigite novamente: ");
+                }
+                while (cFim == 0) {
+                    System.out.println("Nenhum dado encontrado! Digite novamente: ");
+                    cFim = entradaDados.nextInt();
+                }
+                while (cFim < cIni || cFim >= 50) {
+                    System.out.println("Digite um valor a cima de " + cIni + " e até 50: ");
+                    cFim = entradaDados.nextInt();
+                }
+
+            }
         }
         while (tabIni <= tabFim) {
             System.out.println("Tabuada do [" + tabIni + "]");
